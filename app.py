b2f2021ec4854303007b0537bd754a457070881c
Ethan Agranoff <ethana1234@gmail.com>
2020-10-27 15:45:24 -0600

    added cors line

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
+from flask_cors import CORS
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+CORS(app.server, resources={r'/*': {'origins': '*'}})
 server = app.server
